4d97a6af4856b01a395a1f1f7115bebc7d695600
arietwigt <twigt.arie@gmail.com>
2021-07-08 12:30:13 +0200

    added dropdown and completed DataTable

--- autoapp.py
+++ autoapp.py
@@ -1,28 +1,30 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from werkzeug.utils import redirect
-from custom_modules.api_functions import get_car_by_plate, get_random_cars
+from custom_modules.api_functions import get_car_by_plate, get_random_cars, get_brands_list
 
 app = Flask(__name__)
 
 # routes
 @app.route('/')
 def index():
-    redirect
-    return "Hello world"
+    return render_template('index.html')
 
 @app.route('/show_car')
 def show_car():
     car = get_car_by_plate("TB275F")
     
-    return jsonify(car)
+    return render_template('show_car.html', car=car)
 
 
-@app.route('/random_car')
+@app.route('/random_car', methods=['GET', 'POST'])
 def random_car():
-    brand = "AUDI" # <TODO> get from user input
-    car_list = get_random_cars(brand)
-    return render_template('random_auto_tabel.html', car_list=car_list)
-
+    brands_list = get_brands_list()
+    if request.method == 'POST':
+        brand = request.form.get('brand').upper()
+        car_list = get_random_cars(brand)
+        return render_template('random_auto_tabel.html', car_list=car_list, brand=brand, brands_list=brands_list)
+    else:
+        return render_template('random_auto_tabel.html', brands_list=brands_list)
 
 #
 if __name__ == '__main__':
--- custom_modules/api_functions.py
+++ custom_modules/api_functions.py
@@ -13,3 +13,10 @@ def get_random_cars(brand):
     resp_list = resp.json()
     return resp_list
     
+    
+def get_brands_list():
+    url = "https://opendata.rdw.nl/resource/m9d7-ebf2.json?$query=SELECT DISTINCT(merk) WHERE voertuigsoort = 'Personenauto'"
+    resp = requests.get(url)
+    brands_dict = resp.json()
+    brands_list = [list(brand.values())[0] for brand in brands_dict ]
+    return brands_list
